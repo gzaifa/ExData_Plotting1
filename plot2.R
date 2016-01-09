706d4e770650f4a60003d8ab75db871b5381a4ab
zaifa <gzaifa@hotmail.com>
2016-01-09 13:59:11 +0800

    <lab 2 plot2>

---
+++ plot2.R
@@ -0,0 +1,30 @@
+# Read in the data file on the working directory
+power <- read.table(file = "household_power_consumption.txt", header= TRUE, sep=";", na.strings = "?")
+
+# Cast the Date to Date format
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+
+# Subset the required data 2007-02-01 and 2007-02-02
+power_sub <- subset(power, subset=(Date >= "2007-02-01" & Date<="2007-02-02"))
+
+# Check that the subset data is correct.
+# table(power_sub$Date)
+
+# Combine Date and Time
+x <- paste(power_sub$Date, power_sub$Time)
+power_sub$DateTime <- as.POSIXct(x)
+
+# Cast to Time
+
+# Define output device
+png("plot2.png", width = 480, height = 480)
+
+# Construct histogram
+plot(power_sub$Date, power_sub$Global_active_power,
+     type="l", 
+     ylab="Global Active Power (kilowatts)",
+     xlab="")
+
+# Close output device
+dev.off()
+
